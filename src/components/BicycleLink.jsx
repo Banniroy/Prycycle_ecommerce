3c46a10ab2785a77f5cf2145d33c2575d3257f89
banniroy <matinadekola33@gmail.com>
2023-02-21 20:09:35 +0100

    new commit

--- src/components/BicycleLink.jsx
+++ src/components/BicycleLink.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import image_1 from '../img/bicycle_0.jpg';
-import image_2 from '../img/bicycle_2.jpg';
+import image_0 from '../img/bicycle_0.jpg';
+import image_1 from '../img/bicycle_1.jpg';
 
 const BicycleLink = () => {
     return (
@@ -10,14 +10,20 @@ const BicycleLink = () => {
                 <h2 className='hero_2'>Rock it, no matter
                     the dirt
                 </h2>
-                <img className='image_1' src={image_1} height='300' />
+                <img className='image_1' src={image_0} height='300' />
+            </div>
+
+            <div className='menu_link'>
+
             </div>
 
             <div className='bycycleLink_2'>
-                <h3 className='hero_1 hero'>PRYCYLE BIKES</h3>
-                <p className='hero_2 hero'>E-Bikes, Mountain, Graavel, Road & More!</p>
-                <button className='hero'>SHOP NOW</button>
-                <img className='image_1' src={image_2} height='300' />
+                <div className='bycycleLink_2n'>
+                    <h3 className='hero_1'>PRYCYLE BIKES</h3>
+                    <p className='hero_2'>E-Bikes, Mountain, Gravel, Road & More!</p>
+                    <button>SHOP NOW</button>
+                </div>
+                <img className='image_1' src={image_1} height='300' />
             </div>
 
         </div>
