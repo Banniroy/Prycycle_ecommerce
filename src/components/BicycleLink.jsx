9128ff90d09546278057f244534ebfc963173195
banniroy <matinadekola33@gmail.com>
2023-02-22 09:14:19 +0100

    new commit

--- src/components/BicycleLink.jsx
+++ src/components/BicycleLink.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import image_0 from '../img/bicycle_0.jpg';
-import image_1 from '../img/bicycle_1.jpg';
 
 const BicycleLink = () => {
     return (
         <div className='bycycleLink'>
             <div className='bycycleLink_1'>
-                <h2 className='hero_1'>ALL ROUND TRAIL FUN</h2>
-                <h2 className='hero_2'>Rock it, no matter
-                    the dirt
-                </h2>
-                <img className='image_1' src={image_0} height='300' />
+                <div className="container">
+                    <h2 className='hero_1'>ALL ROUND TRAIL FUN</h2>
+                    <h2 className='hero_2'>Rock it, no matter
+                        the dirt
+                    </h2>
+                </div>
             </div>
 
             <div className='menu_link'>
@@ -22,12 +21,13 @@ const BicycleLink = () => {
             </div>
 
             <div className='bycycleLink_2'>
-                <div className='bycycleLink_2n'>
-                    <h3 className='hero_1'>PRYCYLE BIKES</h3>
-                    <p className='hero_2'>E-Bikes, Mountain, Gravel, Road & More!</p>
-                    <button>SHOP NOW &#8594;</button>
+                <div className='container'>
+                    <div className='label-container'>
+                        <h3 className='hero_1'>PRYCYLE BIKES</h3>
+                        <p className='hero_2'>E-Bikes, Mountain, Gravel, Road & More!</p>
+                        <button>SHOP NOW &#8594;</button>
+                    </div>
                 </div>
-                <img className='image_1' src={image_1} height='300' />
             </div>
 
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <header className='header'>
       <section className='header__section'>
-      <section className="head"></section>
+        <section className="head"></section>
         <h1>Prycycle</h1>
         <button className='menuBtn'>
-          <div className='menuBar'></div>
+          <div className='menu-icon'></div>
         </button>
+        {openMenu && (
+          <div className='menu-dropdown'>
+            <a href=""></a>
+          </div>
+        )}
       </section>
     </header>
   )
