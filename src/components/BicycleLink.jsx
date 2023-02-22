8f0750fe60d71abb570158b9aebe14cf705dab3e
banniroy <matinadekola33@gmail.com>
2023-02-22 11:47:54 +0100

    latest comit

--- src/components/BicycleLink.jsx
+++ src/components/BicycleLink.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Ad_1 from '../img/Ad_1.png';
+import Ad_2 from '../img/Ad_2.png';
+import Ad_3 from '../img/Ad_3.jpeg';
 
 const BicycleLink = () => {
     return (
@@ -6,17 +9,28 @@ const BicycleLink = () => {
             <div className='bycycleLink_1'>
                 <div className="container">
                     <h2 className='hero_1'>ALL ROUND TRAIL FUN</h2>
-                    <h2 className='hero_2'>Rock it, no matter
+                    <h2 className='hero_2'>Ride it, no matter
                         the dirt
                     </h2>
                 </div>
             </div>
 
             <div className='menu_link'>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className='bicycle-Ad'>
+                    <img src={Ad_1} alt="" width='113' height='90'/>
+                    <p>COMPLETE BIKES</p>
+                </div>
+                <div className='bicycle-Ad'>
+                    <img src={Ad_2} alt="" width='150' height='90'/>
+                    <p>SKATEBOARDS</p>
+                </div>
+                <div className='bicycle-Ad'>
+                    <img src={Ad_3} alt="" width='113' height='100'/>
+                    <p>COMPONENTS & ACCESSORIES</p>
+                </div>
+                <div className='sales'>
+                    <p>SALES</p>
+                </div>
 
             </div>
 
@@ -27,6 +41,12 @@ const BicycleLink = () => {
                         <p className='hero_2'>E-Bikes, Mountain, Gravel, Road & More!</p>
                         <button>SHOP NOW &#8594;</button>
                     </div>
+                </div>
+            </div>
+
+            <div className="skateboard">
+                <div className='container'>
+                    <p>COMMING SOON</p>
                 </div>
             </div>
 
