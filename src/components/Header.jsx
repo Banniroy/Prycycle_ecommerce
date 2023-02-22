b0408b44c5c671091c25bad9bab737bfdae493c7
banniroy <matinadekola33@gmail.com>
2023-02-22 12:59:46 +0100

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,15 @@ const Header = ({ openMenu, setOpenMenu }) => {
       <section className='header__section'>
         <section className="head"></section>
         <h1>Prycycle</h1>
-        <button className='menuBtn'>
+        <button 
+          className='menuBtn'
+          onClick={handleMenuClick}
+        >
           <div className='menu-icon'></div>
         </button>
         {openMenu && (
           <div className='menu-dropdown'>
+            <a href=""></a>
             <a href=""></a>
           </div>
         )}
