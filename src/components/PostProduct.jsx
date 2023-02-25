1cdb12eced96951cb5453205454d24e16813ce09
banniroy <matinadekola33@gmail.com>
2023-02-25 16:11:22 +0100

    new commit

--- src/components/PostProduct.jsx
+++ src/components/PostProduct.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '../Css/PostProduct.css';
 
 const PostProduct = () => {
   return (
-    <div className='PostProduct'>PostProduct</div>
+    <section className='PostProduct'>
+      <form action="" className='form'>
+        <label className='label_1' htmlFor="">Choose a name for your product</label>
+        <input
+          className='input_info'
+          type="text"
+          name=""
+          id=""
+          placeholder=''
+        />
+        <label
+          className='info_product'
+        >Information about your product</label>
+        <textarea></textarea>
+        <div className="container">
+          <input type="file" />
+        </div>
+        <label htmlFor="">Price</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder='$...'
+        />
+        <button>Submit</button>
+      </form>
+    </section>
   )
 }
 
