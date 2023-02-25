e90c8317d0e46b8450883bcfd3d91036137099dd
banniroy <matinadekola33@gmail.com>
2023-02-25 11:00:37 +0100

    new commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import '../Css/Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUser, faPlus, faBars, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import '../Css/Footer.css';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
+      <Link className='link' to="/">
+        <section>
+          <FontAwesomeIcon className='font' icon={faHouse} />
+        </section>
+      </Link>
+      <Link className='link' to="/menu">
+        <section>
+          <FontAwesomeIcon className='font' icon={faBars} />
+        </section>
+      </Link>
+      <Link className='link' to="/postProduct">
+        <section>
+          <FontAwesomeIcon className='font' icon={faPlus} />
+        </section>
+      </Link>
+      <Link className='link' to="/shoppingCart">
+        <section>
+          <FontAwesomeIcon className='font' icon={faCartShopping} />
+        </section>
+      </Link>
+      <Link className='link' to="/profile">
+        <section>
+          <FontAwesomeIcon className='font' icon={faUser} />
+        </section>
+      </Link>
     </footer>
   )
 }
