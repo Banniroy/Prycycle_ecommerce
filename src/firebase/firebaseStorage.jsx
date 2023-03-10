8a7bd2ed953ac9493e9619250f2cd349cf82c462
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-03-10 10:53:37 +0100

    Update firebaseStorage.jsx

--- src/firebase/firebaseStorage.jsx
+++ src/firebase/firebaseStorage.jsx
@@ -2,14 +2,14 @@ import axios from 'axios';
 import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD96PRhxJmIM3LDI0EQuaqKw9tirBT7STo",
-  authDomain: "prycycle-website.firebaseapp.com",
-  projectId: "prycycle-website",
-  storageBucket: "prycycle-website.appspot.com",
-  messagingSenderId: "851668516273",
-  appId: "1:851668516273:web:754ced65fde68db8465190",
-  measurementId: "G-27VC6YXK6M"
+  apiKey: "AIzaSyCDQ01JsSP-H9LAeDx_xZXKCu6oZoWhyt0",
+  authDomain: "bicycle-website-3a0d9.firebaseapp.com",
+  projectId: "bicycle-website-3a0d9",
+  storageBucket: "bicycle-website-3a0d9.appspot.com",
+  messagingSenderId: "42393516978",
+  appId: "1:42393516978:web:f178a71389b86ac88342ab"
 };
 
 // Initializing Firebase
